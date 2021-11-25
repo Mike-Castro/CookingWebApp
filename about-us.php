621997aefc4bc1bc57e9d4c1bc71325db146138f
adriangzz <adriangonzalezmontemayor@gmail.com>
2021-11-25 11:32:41 -0600

    Remove body from about

--- about-us.php
+++ about-us.php
@@ -1,47 +1,44 @@
 <?php include 'header.php'; ?>
 <hr class="hr1">
+<h1>About Us</h1>
+<p>We are students from Instituto Tecnológico y de Estudios Superiores de Monterrey, this is a project for our web
+  applications development class, in which we apply every subject we learned.</p>
+</div>
 
-<body>
-  <div class="about-section">
-    <h1>About Us</h1>
-    <p>We are students from Instituto Tecnológico y de Estudios Superiores de Monterrey, this is a project for our web
-      applications development class, in which we apply every subject we learned.</p>
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team1.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Miguel Castro</h2>
+        <p class="title">Software Engineer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
   </div>
 
-  <h2 style="text-align:center">Our Team</h2>
-  <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="/w3images/team1.jpg" alt="Mike" style="width:100%">
-        <div class="container">
-          <h2>Miguel Castro</h2>
-          <p class="title">Software Engineer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        </div>
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team2.jpg" alt="Adrian" style="width:100%">
+      <div class="container">
+        <h2>Adrian Gonzalez</h2>
+        <p class="title">Software Engineer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
       </div>
     </div>
-
-    <div class="column">
-      <div class="card">
-        <img src="/w3images/team2.jpg" alt="Adrian" style="width:100%">
-        <div class="container">
-          <h2>Adrian Gonzalez</h2>
-          <p class="title">Software Engineer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="column">
-      <div class="card">
-        <img src="/w3images/team3.jpg" alt="Isaac" style="width:100%">
-        <div class="container">
-          <h2>Isaac Jordan</h2>
-          <p class="title">Software Engineer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        </div>
-      </div>
-    </div>
-    <link rel="stylesheet" href="./Resources/css/about_style.css">
   </div>
-  <?php include 'footer.php'; ?>
+
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team3.jpg" alt="Isaac" style="width:100%">
+      <div class="container">
+        <h2>Isaac Jordan</h2>
+        <p class="title">Software Engineer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+  <link rel="stylesheet" href="./Resources/css/about_style.css">
+</div>
+<?php include 'footer.php'; ?>
