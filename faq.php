c593376caff9aaecd23b75afd744c8b57f685f01
adriangzz <adriangonzalezmontemayor@gmail.com>
2021-11-28 22:35:10 -0600

    Fix faq dropdown

--- faq.php
+++ faq.php
@@ -2,7 +2,7 @@
 <div style="text-align:center;">
     <h1>FAQ'S</h1>
     <button class="accordion">Can you post guest recipes?</button>
-    <div class="panel">
+    <div class="panel" style="display:none;">
         <p>Actually, we do! In the contact page you can send us your recipe with all the instructions,
             we will try it and if we like it, we will post it!
         </p>
@@ -10,17 +10,17 @@
     <hr class="hr-line">
 
     <button class="accordion">It's okay if I post a recipe from EasyCooking in my own blog?</button>
-    <div class="panel">
+    <div class="panel" style="display:none;">
         <p>Yes! No problem, just remember to give us credit.</p>
     </div>
     <hr class="hr-line">
 
     <button class="accordion">Can I substitute an ingredient from a recipe?</button>
-    <div class="panel">
+    <div class="panel" style="display:none;">
         <p>We don't recommend it since all the recipes are delicious with the marked ingredients,
             but if you think it will work, do it!</p>
     </div>
-    <script type="text/javascript" src="./Resources/js/cookapp.js"></script>
+    <hr class="hr-line">
     <link rel="stylesheet" href="./Resources/css/faq_style.css">
 </div>
 <?php include 'footer.php'; ?>
